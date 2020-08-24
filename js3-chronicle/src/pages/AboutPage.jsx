c4741de5a173ac08bae50846c45d2a2c2bc81cf7
Dmitrijv <dmitrij.vel@live.se>
2020-08-24 18:17:23 +0200

    updated about section

--- js3-chronicle/src/pages/AboutPage.jsx
+++ js3-chronicle/src/pages/AboutPage.jsx
@@ -3,14 +3,16 @@ import React from "react";
 export default function AboutPage() {
   return (
     <div className="about-section">
+      <h5>FEND19 - JavaScript 3 - USA Chronicle</h5>
       <h6>Dmitrij Velström, Nackademin</h6>
       <h6>
         <time>2020-08-22</time>
       </h6>
       <br />
       <p>
-        Welcome to USA Chronicles, a React app that displays data from a newspaper archive{" "}
-        <a href="https://chroniclingamerica.loc.gov/about/api/#linked-data">chroniclingamerica.loc.gov</a>.
+        USA Chronicle is a React app that displays API data from{" "}
+        <a href="https://chroniclingamerica.loc.gov/about/api/#linked-data">chroniclingamerica.loc.gov</a>, a website
+        that provides access to information about historic newspapers.
       </p>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat cumque facere alias in officiis eum
